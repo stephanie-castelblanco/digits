c8e0af1eb012f0be404738b9eb2b4da32d7e53bd
Stephanie Castelblanco <stephanye0523@hotmail.com>
2024-03-29 18:18:05 -1000

    1

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Contacts } from '../../api/contact/Contacts';
 
 /* eslint-disable no-console */
 
@@ -14,5 +15,19 @@ if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.forEach(data => addData(data));
+  }
+}
+
+// Initialize the database with default contact data.
+const addContact = (contact) => {
+  console.log(`  Adding: ${contact.lastName} (${contact.owner})`);
+  Contacts.collection.insert(contact);
+};
+
+// Initialize the Stuff collection if empty.
+if (Contacts.collection.find().count() === 0) {
+  if (Meteor.settings.defaultContacts) {
+    console.log('Creating default contact data.');
+    Meteor.settings.defaultContacts.forEach(contact => addContact(contact));
   }
 }
