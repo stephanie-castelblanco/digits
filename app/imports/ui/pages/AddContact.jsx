02fe7885941bfc6c2ad36b547b31307157d5269d
Stephanie Castelblanco <stephanye0523@hotmail.com>
2024-03-29 22:22:09 -1000

    1

--- app/imports/ui/pages/AddContact.jsx
+++ app/imports/ui/pages/AddContact.jsx
@@ -1,34 +1,32 @@
 import React from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import { AutoForm, ErrorsField, NumField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { AutoForm, ErrorsField, LongTextField, SubmitField, TextField } from 'uniforms-bootstrap5';
 import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
-import { Stuffs } from '../../api/stuff/Stuff';
+import { Contacts } from '../../api/contact/Contacts';
 
 // Create a schema to specify the structure of the data to appear in the form.
 const formSchema = new SimpleSchema({
-  name: String,
-  quantity: Number,
-  condition: {
-    type: String,
-    allowedValues: ['excellent', 'good', 'fair', 'poor'],
-    defaultValue: 'good',
-  },
+  firstName: String,
+  lastName: String,
+  address: String,
+  image: String,
+  description: String,
 });
 
 const bridge = new SimpleSchema2Bridge(formSchema);
 
 /* Renders the AddStuff page for adding a document. */
-const AddStuff = () => {
+const AddContact = () => {
 
   // On submit, insert the data.
   const submit = (data, formRef) => {
-    const { name, quantity, condition } = data;
+    const { firstName, lastName, address, image, description } = data;
     const owner = Meteor.user().username;
-    Stuffs.collection.insert(
-      { name, quantity, condition, owner },
+    Contacts.collection.insert(
+      { firstName, lastName, address, image, description, owner },
       (error) => {
         if (error) {
           swal('Error', error.message, 'error');
@@ -45,15 +43,21 @@ const AddStuff = () => {
   return (
     <Container className="py-3">
       <Row className="justify-content-center">
-        <Col xs={5}>
-          <Col className="text-center"><h2>Add Stuff</h2></Col>
+        <Col xs={10}>
+          <Col className="text-center"><h2>Add Contact</h2></Col>
           <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
             <Card>
               <Card.Body>
-                <TextField name="name" />
-                <NumField name="quantity" decimal={null} />
-                <SelectField name="condition" />
-                <SubmitField value="Submit" />
+                <Row>
+                  <Col><TextField name="firstName" /></Col>
+                  <Col><TextField name="lastName" /></Col>
+                </Row>
+                <Row>
+                  <Col><TextField name="address" /></Col>
+                  <Col><TextField name="image" /></Col>
+                </Row>
+                <LongTextField name="description" />
+                <SubmitField />
                 <ErrorsField />
               </Card.Body>
             </Card>
@@ -64,4 +68,4 @@ const AddStuff = () => {
   );
 };
 
-export default AddStuff;
+export default AddContact;
