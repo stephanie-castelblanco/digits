61b8c973cbf19db209b79f4eac3a695a1d0af611
Stephanie Castelblanco <stephanye0523@hotmail.com>
2024-03-29 18:29:54 -1000

    1

--- app/imports/ui/pages/ListContactsAdmin.jsx
+++ app/imports/ui/pages/ListContactsAdmin.jsx
@@ -4,16 +4,16 @@ import { Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Contacts } from '../../api/contact/Contacts';
 import LoadingSpinner from '../components/LoadingSpinner';
-import Contact from '../components/Contact';
+import ContactAdmin from '../components/ContactAdmin';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-const ListContacts = () => {
+const ListContactsAdmin = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { ready, contacts } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Contacts.userPublicationName);
+    const subscription = Meteor.subscribe(Contacts.adminPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
     // Get the Stuff documents
@@ -34,7 +34,7 @@ const ListContacts = () => {
           <Row xs={1} md={2} lg={3} className="g-4">
             {contacts.map((contact) => (
               <Col key={contact.id}>
-                <Contact contact={contact} />
+                <ContactAdmin contact={contact} />
               </Col>
             ))}
           </Row>
@@ -44,4 +44,4 @@ const ListContacts = () => {
   ) : <LoadingSpinner />);
 };
 
-export default ListContacts;
+export default ListContactsAdmin;
