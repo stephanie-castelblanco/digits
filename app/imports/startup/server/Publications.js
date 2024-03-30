8ca29fb91cf9233b8ae9280fb6a28f88ac1945de
Stephanie Castelblanco <stephanye0523@hotmail.com>
2024-03-29 18:18:37 -1000

    1

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,9 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
+import { Contacts } from '../../api/contact/Contacts';
 
-// User-level publication.
-// If logged in, then publish documents owned by this user. Otherwise, publish nothing.
+// User-level publication for Stuffs.
 Meteor.publish(Stuffs.userPublicationName, function () {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
@@ -12,11 +12,27 @@ Meteor.publish(Stuffs.userPublicationName, function () {
   return this.ready();
 });
 
-// Admin-level publication.
-// If logged in and with admin role, then publish all documents from all users. Otherwise, publish nothing.
+// User-level publication for Stuff.
+Meteor.publish(Contacts.userPublicationName, function () {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return Contacts.collection.find({ owner: username });
+  }
+  return this.ready();
+});
+
+// Admin-level publication for Stuffs.
 Meteor.publish(Stuffs.adminPublicationName, function () {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Stuffs.collection.find();
+  }
+  return this.ready();
+});
+
+// Admin-level publication for Stuff.
+Meteor.publish(Contacts.adminPublicationName, function () {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return Contacts.collection.find();
   }
   return this.ready();
 });
