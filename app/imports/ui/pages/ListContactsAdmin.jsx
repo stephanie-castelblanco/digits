603137bbf1bd4af17abb8f07223a044362fbb8df
Stephanie Castelblanco <stephanye0523@hotmail.com>
2024-03-29 17:04:32 -1000

    1

--- app/imports/ui/pages/ListContactsAdmin.jsx
+++ app/imports/ui/pages/ListContactsAdmin.jsx
@@ -6,10 +6,17 @@ import { Contacts } from '../../api/contact/Contacts';
 import LoadingSpinner from '../components/LoadingSpinner';
 import ContactAdmin from '../components/ContactAdmin';
 
+/* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const ListContactsAdmin = () => {
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { ready, contacts } = useTracker(() => {
+    // Note that this subscription will get cleaned up
+    // when your component is unmounted or deps change.
+    // Get access to Stuff documents.
     const subscription = Meteor.subscribe(Contacts.adminPublicationName);
+    // Determine if the subscription is ready
     const rdy = subscription.ready();
+    // Get the Stuff documents
     const contactItems = Contacts.collection.find({}).fetch();
     return {
       contacts: contactItems,
