6e9861831d11a45add5c84ff315f8f472560b443
Stephanie Castelblanco <stephanye0523@hotmail.com>
2024-03-30 15:53:45 -1000

    1

--- app/imports/ui/pages/ListStuff.jsx
+++ app/imports/ui/pages/ListStuff.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Col, Container, Row, Table } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
-import { Contacts } from '../../api/stuff/Stuff';
+import { Contacts } from '../../api/contact/Contacts';
 import StuffItem from '../components/StuffItem';
 import LoadingSpinner from '../components/LoadingSpinner';
 
